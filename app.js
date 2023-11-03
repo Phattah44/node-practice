0d2ed90a9495a3efd445b1627ef37a118d7472ee
Abdulfattah Sheu <elsheikh.saf@gmail.com>
2023-11-03 02:40:52 +0100

    second

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const http = require('http');
 const { readFileSync } = require('fs');
 
-const homepage = readFileSync('/index.html');
+const navbar = readFileSync('./navbar-app/index.html');
+const navstyle = readFileSync('./navbar-app/styles.css');
+const navlogo = readFileSync('./navbar-app/logo.svg');
+const navlogic = readFileSync('./navbar-app/browser-app.js');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
 
   if (url === '/') {
     res.writeHead(200, { 'content-type': 'text/html' });
-    res.write(homepage);
+    res.write(navbar);
     res.end();
   } else if (url === '/about') {
     res.writeHead(200, { 'content-type': 'text/html' });
     res.write('<h1>About Page</h1>');
+    res.end();
+  } else if (url === '/styles.css') {
+    res.writeHead(200, { 'content-type': 'text/css' });
+    res.write(navstyle);
+    res.end();
+  } else if (url === '/logo.svg') {
+    res.writeHead(200, { 'content-type': 'image/svg+xml' });
+    res.write(navlogo);
+    res.end();
+  } else if (url === '/browser-app.js') {
+    res.writeHead(200, { 'content-type': 'text/javascript' });
+    res.write(navlogic);
     res.end();
   } else {
     res.writeHead(404, { 'content-type': 'text/html' });
